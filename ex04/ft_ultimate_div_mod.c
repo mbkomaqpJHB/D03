827fa7a9faed3f55465f643b4bc6d99a83e61c40
Mbali Komaqpe <mbkomaqp@c5r8s8.wethinkcode.co.za>
2020-07-12 11:11:18 +0200

    ft_ultimate_div_mod

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -6,23 +6,15 @@
 /*   By: mbkomaqp <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/10 15:45:29 by mbkomaqp          #+#    #+#             */
-/*   Updated: 2020/07/10 15:54:31 by mbkomaqp         ###   ########.fr       */
+/*   Updated: 2020/07/12 11:10:19 by mbkomaqp         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
 	int temp;
+
 	temp = *a;
 	*a = *a / *b;
 	*b = temp % *b;
-}
-
-int	main()
-{
-	int a = 20, b = 10;
-	ft_ultimate_div_mod(&a, &b);
-	printf("%d %d \n", a, b);
 }
